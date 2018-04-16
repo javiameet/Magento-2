51e59e706ec65b203365145b0cb2c91d2026d454
Meet Javiya <meetjavia007@gmail.com>
2018-04-16 21:19:54 +0530

    Create Simple Module in Magento 2
    Create Simple Module in Magento 2

---
+++ app/code/Nisl/Helloworld/Block/Helloworld.php
@@ -0,0 +1,9 @@
+<?php
+namespace Nisl\Helloworld\Block;
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
